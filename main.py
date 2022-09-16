25a2362bf199e2f0f0d6500456d04e07448f7a3a
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-09-16 20:06:28 +0000

    slicing

--- main.py
+++ main.py
@@ -93,7 +93,10 @@ text3 = "It's great to work with computers. They don't argue, they remember ever
 print(text3[::-1])
 
 # Extract the first word of the following sentence using slicing, and display it on the screen:
-"Controlling complexity is the essence of programming"
+sentence = "Controlling complexity is the essence of programming"
+all_words = sentence.split()
+print(all_words)
+print("first word is : " + all_words[0])
 
 # create a text input that asks for choice of food
 #create an empty list variable called food
